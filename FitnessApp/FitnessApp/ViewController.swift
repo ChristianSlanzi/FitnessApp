091a4a11f0cb529060b56f91320ee272362180e6
Christian Slanzi <christian.slanzi@gmail.com>
2020-10-25 11:36:37 +0100

    xcode project created

---
+++ FitnessApp/FitnessApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitnessApp
+//
+//  Created by Christian Slanzi on 25.10.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
