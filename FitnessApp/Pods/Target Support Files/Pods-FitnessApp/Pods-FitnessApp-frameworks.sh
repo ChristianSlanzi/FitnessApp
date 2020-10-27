9a20cbd68771c3e50fed803ad56c2c64b23ecf1c
Christian Slanzi <christian.slanzi@gmail.com>
2020-10-27 19:42:48 +0100

    add Overture pod

--- FitnessApp/Pods/Target Support Files/Pods-FitnessApp/Pods-FitnessApp-frameworks.sh
+++ FitnessApp/Pods/Target Support Files/Pods-FitnessApp/Pods-FitnessApp-frameworks.sh
@@ -163,10 +163,12 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/CS_Common_UI/CS_Common_UI.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/CS_Common_Utils/CS_Common_Utils.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Overture/Overture.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/CS_Common_UI/CS_Common_UI.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/CS_Common_Utils/CS_Common_Utils.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Overture/Overture.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # FitnessApp
 
+A sample project to demostrate the usage of clean code techniques.
+
 ### Status
 [![Build Status](https://travis-ci.com/ChristianSlanzi/FitnessApp.svg?branch=main)](https://travis-ci.com/ChristianSlanzi/FitnessApp)
